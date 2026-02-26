041e27258e3dfffc148d797604853d57874ab2d9
Reina <ninasuicox@gmail.com>
2026-02-26 16:25:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "/REPO/",
+  base: "/romance-archives/",
   plugins: [react()],
 })
